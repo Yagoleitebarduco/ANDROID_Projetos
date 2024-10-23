d3236885a51dda62fbb67a286bc4cdcf07b55ac6
Yago <yago.g4c@gmail.com>
2024-10-23 14:29:39 -0300

    Finalizado

--- Projeto06/app/src/main/java/com/yagoleitebarduco/projeto06/MainActivity.java
+++ Projeto06/app/src/main/java/com/yagoleitebarduco/projeto06/MainActivity.java
@@ -1,12 +1,16 @@
 package com.yagoleitebarduco.projeto06;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    String[] frases = {
+            "A jornada pode ser longa, mas cada passo é um avanço em direção aos seus sonhos.",
+            "A força não vem de vencer sempre, mas de levantar-se toda vez que cair.",
+            "Acredite em você e faça do seu esforço a ponte entre o sonho e a realidade.",
+            "Beba Agua!!"
+    };
+
+    public void gerarFrase (View view) {
+        TextView texto = findViewById(R.id.txt_result);
+        int nAleatorio = new Random().nextInt(4);
+
+        String frase = frases[nAleatorio];
+
+        texto.setText(frase);
+    }
+
+    public void exibirFrases (View view) {
+        TextView texto = findViewById(R.id.txt_result);
+        String txtresult = "";
+
+        for (String frase : frases) {
+            txtresult = txtresult + frase + "\n \n";
+        }
+
+        texto.setText(txtresult);
     }
 }
