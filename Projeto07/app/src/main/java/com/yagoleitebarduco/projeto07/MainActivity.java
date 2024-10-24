605ea9011e7bbfa2735e75fff3b6a59267146ebf
Yago <yago.g4c@gmail.com>
2024-10-24 15:17:03 -0300

    Finalizado

--- Projeto07/app/src/main/java/com/yagoleitebarduco/projeto07/MainActivity.java
+++ Projeto07/app/src/main/java/com/yagoleitebarduco/projeto07/MainActivity.java
@@ -1,12 +1,17 @@
 package com.yagoleitebarduco.projeto07;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,69 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void selecionarPedra(View view) {
+        verificarGanhador("Pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        verificarGanhador("Papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        verificarGanhador("Tesoura");
+    }
+
+    private String escolhaAleatoriaApp(){
+        String[] opcoes = {
+                "Pedra",
+                "Papel",
+                "Tesoura"
+        };
+
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView image_app = findViewById(R.id.img_app);
+        String escolhaApp = opcoes[numeroAleatorio];
+
+        switch (escolhaApp) {
+            case "Pedra":
+                image_app.setImageResource(R.drawable.pedra);
+                break;
+            case "Papel":
+                image_app.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                image_app.setImageResource(R.drawable.tesoura);
+                break;
+        };
+
+        return escolhaApp;
+    }
+
+
+    public void verificarGanhador(String escolhaDoUsuario) {
+        // Testar se o Botão esta Pegando --> System.out.println("Item Clicado: " + escolhaDoUsuario);
+
+        String escolhaApp = escolhaAleatoriaApp();
+        TextView txt_result = findViewById(R.id.txt_result);
+
+        if ( //APP GANHAR
+                (escolhaApp == "Pedra" && escolhaDoUsuario == "Tesoura") ||
+                (escolhaApp == "Papel" && escolhaDoUsuario == "Pedra") ||
+                (escolhaApp == "Tesoura" && escolhaDoUsuario == "Papel")) {
+
+            txt_result.setText("Você Perdeu :(");
+        } else if (//USUARIO GANHAR
+                (escolhaApp == "Pedra" && escolhaDoUsuario == "Papel") ||
+                (escolhaApp == "Papel" && escolhaDoUsuario == "Tesoura") ||
+                (escolhaApp == "Tesoura" && escolhaDoUsuario == "Pedra")) {
+
+            txt_result.setText("Você Ganhou :)");
+        } else {
+            // EMPATAE
+            txt_result.setText("Você Empatou");
+        }
     }
 }
