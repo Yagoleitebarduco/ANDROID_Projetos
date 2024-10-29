a8f44424cf008b068ee508f53572de661e9ea7d0
Yago <yago.g4c@gmail.com>
2024-10-29 16:34:21 -0300

    Finalizado

---
+++ Projeto10/app/src/main/java/com/yagoleitebarduco/projeto10/ImcActivity.java
@@ -0,0 +1,120 @@
+package com.yagoleitebarduco.projeto10;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ImcActivity extends AppCompatActivity {
+
+    private TextInputEditText editPeso, editAltura;
+    private Button btnCalc, btnLimp;
+    double imc, peso, altura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_imc);
+
+        // TextInputLayout
+        editPeso = findViewById(R.id.edit_Peso);
+        editAltura = findViewById(R.id.edit_Altura);
+
+        // Botões
+        btnCalc = findViewById(R.id.btn_calcImc);
+        btnLimp = findViewById(R.id.btn_limpar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void calcImc(View view) {
+        peso = (Double.parseDouble(editPeso.getText().toString()));
+        altura = (Double.parseDouble(editAltura.getText().toString()));
+
+        altura = altura / 100;
+
+        imc = peso / (altura * altura);
+
+        if (imc < 17) {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Muito Abaixo do Peso!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else if (imc >= 17 && imc < 18.5) {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Abaixo do Peso!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else if (imc >= 18.5 && imc< 25) {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Peso Normal!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else if (imc >= 25 && imc < 30){
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Acima do Peso!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else if (imc >= 30 && imc < 35) {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Obesida I");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else if (imc >= 35 && imc <40) {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Obesidade II - Severa!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        } else {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+            alerta.setTitle("Calculadora IMC");
+            alerta.setMessage("Seu IMC e de " + imc + " - Obesidade II - Morbido!");
+            alerta.setNeutralButton("CONFIRMAR", null);
+
+            alerta.create();
+            alerta.show();
+        }
+    }
+
+    public void limpar(View view) {
+
+    }
+}
--- Projeto10/app/src/main/java/com/yagoleitebarduco/projeto10/MainActivity.java
+++ Projeto10/app/src/main/java/com/yagoleitebarduco/projeto10/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yagoleitebarduco.projeto10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView btnTelaImc, btnTelaCalcGas, btnTelaCalc, btnOutros;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnTelaImc = findViewById(R.id.app_imc);
+        btnTelaCalcGas = findViewById(R.id.app_calcaGasolina);
+        btnTelaCalc = findViewById(R.id.app_Calculadora);
+        btnOutros = findViewById(R.id.app_Outros);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void abrirAppImc (View view) {
+        Intent telaImc = new Intent(MainActivity.this, ImcActivity.class);
+        startActivity(telaImc);
     }
 }
